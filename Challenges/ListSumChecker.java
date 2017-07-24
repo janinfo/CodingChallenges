a2e4efe63c3721aa4acc6d50c373043485e65657
janinfo <j.k1994@gmx.de>
2017-07-24 19:59:56 +0200

    Comments for more understanding

--- Challenges/ListSumChecker.java
+++ Challenges/ListSumChecker.java
@@ -3,8 +3,10 @@ import java.util.*;
 public class ListSumChecker {
 	public static void main(String[] args) {
 		
-		Set<Integer> myNumList = new HashSet<Integer>(); // hashSet for actual numbers
-		Set<Integer> inversList = new HashSet<Integer>(); // hashSet for invers numbers
+		// hashSet for actual numbers
+		Set<Integer> myNumList = new HashSet<Integer>();
+		// hashSet for invers numbers
+		Set<Integer> inversList = new HashSet<Integer>();
 		
 		int myInt;
 		
@@ -15,19 +17,24 @@ public class ListSumChecker {
 		System.out.println("Enter a valid Integer to add to the hashSet");
 		System.out.println("(Hint: add '0' to stop scaning)");
 		
+		// scan as many integers as you want; end scanning with '0'
 		int nextListInt;
 		do {
 		Scanner scanList = new Scanner(System.in);
 		nextListInt = scanList.nextInt();
 		myNumList.add(nextListInt);
-		} while (0 < nextListInt && nextListInt < 99 );
+		} while (nextListInt != 0);
 		
+
+		// '0' is end condition and not used in the hastSet
 		myNumList.remove(0);
 		
 		System.out.println("The list to check:");
 		System.out.println(myNumList + "\n");
 
+		// iterate through myNumList
 		Iterator myIterator = myNumList.iterator();
+		// add the number to inversList if its not already included
 		while (myIterator.hasNext()) {
 			myInt = (Integer) myIterator.next();
 			if (inversList.contains(myInt)) {
