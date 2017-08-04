50cc7ce4c65263b9cb5a53f4ae1d1447c2cdf5c9
janinfo <j.k1994@gmx.de>
2017-08-04 18:44:56 +0200

    removed unnecessary System.out's

--- Challenges/PrimeNumbers.java
+++ Challenges/PrimeNumbers.java
@@ -14,24 +14,25 @@ public class PrimeNumbers {
         TreeSet<Integer> myPrimeNumbers = new TreeSet<Integer>();
 
         // the maximum number you want to verify
-        int max = 100;
+        int max = 10000;
 
         for (int num = 0; num <= max; num++) {
             for (int j = 2; j <= num; j++) {
                 if (num % j == 0) {
-                    System.out.println(num + " is not a prime number.");
+//                    System.out.println(num + " is not a prime number.");
                     break;
                 }
                 if (num - j == 1) {
-                    System.out.println("PRIME NUMBER FOUND! It's: " + num + ".");
+//                    System.out.println("PRIME NUMBER FOUND! It's: " + num + ".");
                     myPrimeNumbers.add(num);
                 }
             }
 
         }
         System.out.println("\n");
-        System.out.println("The complete List of prime numbers from 1 to " + max + " is:");
+        System.out.println("The complete List of prime numbers from 3 to " + max + " is:");
         System.out.println(myPrimeNumbers);
+//        System.out.println("The biggest prime number below " + max + " is " + myPrimeNumbers.last() + ".");
 
     }
 }
