676c9e6fe715066323c2441e75e03f1ddf54db90
janinfo <j.k1994@gmx.de>
2017-08-04 19:00:27 +0200

    Countdown challenge created

---
+++ Challenges/Countdown.java
@@ -0,0 +1,11 @@
+public class Countdown {
+    public static void main (String[] args) {
+
+        /*
+         * _the challenge_
+         *
+         * https://www.reddit.com/r/dailyprogrammer/comments/6fe9cv/20170605_challenge_318_easy_countdown_game_show/
+         */
+        System.out.println("Hello Countdown");
+    }
+}
