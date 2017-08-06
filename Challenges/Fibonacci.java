d46b0b92cab8f4b7b81320bac49fd90dd907ab3d
janinfo <j.k1994@gmx.de>
2017-08-06 23:15:35 +0200

    sometestings

--- Challenges/Fibonacci.java
+++ Challenges/Fibonacci.java
@@ -1,5 +1,21 @@
 public class Fibonacci {
     public static void main (String[] args) {
-        System.out.println("Fibonacci");
+
+        /*
+         * _the challenge_
+         *
+         * write a basic Fibonacci algorithm
+         * the first numbers are: 0 1 1 2 3 5 8 13..
+         */
+//        int a = 0;
+//        int b = 1;
+//
+//        for (int i = 0; i < 10; i++) {
+//            System.out.println(a);
+//            b += a;
+//            a = b;
+//
+//        }
+
     }
 }
